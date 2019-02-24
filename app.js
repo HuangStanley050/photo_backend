c85bba62da56f16337e9371bf55890191fcc47b3
Stanley Huang <infamous_godhand@yahoo.com>
2019-02-24 08:40:54 +0000

    change connection order from mongo first then app listen

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const logger = require("morgan");
 const cors = require("cors");
 const passport = require("passport");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 const upLoadRoute = require("./routes/upload");
 const authRouter = require("./routes/auth");
 const fileRouter = require("./routes/file");
 
-const config = require("./config/config");
+//const config = require("./config/config");
 
-mongoose
-  .connect(config.connection, { useNewUrlParser: true })
-  .then(() => console.log("connected to Atlas"))
-  .catch(err => console.log(err));
+// mongoose
+//   .connect(config.connection, { useNewUrlParser: true })
+//   .then(() => console.log("connected to Atlas"))
+//   .catch(err => console.log(err));
 
 const app = express();
 
